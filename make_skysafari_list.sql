fa82085bd2c80fc4e097e79f0980df28a0280e4d
Aaron K. Johnson <aaron@untwelve.org>
2022-01-23 14:28:13 -0700

    adding the make_skysafari_list.sql script

---
+++ make_skysafari_list.sql
@@ -0,0 +1,9 @@
+.output export.skylist
+SELECT 'SkySafariObservingListVersion=3.0';
+SELECT 'SkyObject=BeginObject
+      ObjectID=2,-1,-1
+      CatalogNumber=WDS ' || wds_num
+      || '
+EndObject=SkyObject'
+FROM tight_doubles;  -- change this to target ones of the preset views
+                     -- or to target a view you've set up in `dbsetup.sql`.
